006046cdd3050e518a9fdeb567bc5d754447b887
elkinpabon <elkindres2@gmail.com>
2025-12-13 20:57:42 -0500

    Update: Limpiar código de prueba

--- cliente/src/test/vulnerableCode.js
+++ cliente/src/test/vulnerableCode.js
@@ -1,37 +1,27 @@
-/**
- * Código vulnerable para pruebas de detección ML
- */
-
-// SQL Injection
 function getUserById(userId) {
     const query = "SELECT * FROM users WHERE id = " + userId;
     return db.execute(query);
 }
 
-// Code Injection
 function evaluateCode(input) {
     return eval(input);
 }
 
-// XSS - HTML Sin sanitizar
 function renderComment(comment) {
     document.getElementById('comments').innerHTML = "<p>" + comment + "</p>";
 }
 
-// Path Traversal
 function readFile(filename) {
     const fs = require('fs');
     return fs.readFileSync('/uploads/' + filename);
 }
 
-// Deserialización insegura
 function parseData(data) {
     const obj = JSON.parse(data);
     eval(obj.code);
     return obj;
 }
 
-// Input validation inseguro
 function saveUser(userData) {
     const user = {
         name: userData.name,
@@ -42,7 +32,6 @@ function saveUser(userData) {
     db.save(user);
 }
 
-// Variables sin inicializar
 function getConfig(key) {
     let value;
     if (config[key]) {
@@ -51,7 +40,6 @@ function getConfig(key) {
     return value;
 }
 
-// Null pointer
 function processUser(userId) {
     const user = getUser(userId);
     const name = user.name;
